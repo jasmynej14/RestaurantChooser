1cef1d6ea1a005f47a45b456df8669ef4f97d67b
jasmynej14 <jasmynej14@brandeis.edu>
2021-10-04 00:56:59 -0400

    add tab navigation

--- App.js
+++ App.js
@@ -2,18 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import SearchScreen from './screens/SearchScreen';
 import AboutScreen from './screens/AboutScreen';
+import Icon from 'react-native-vector-icons/Ionicons'
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen}/>
-          <Stack.Screen name="Search" component={SearchScreen}/>
-          <Stack.Screen name="About" component={AboutScreen}/>
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen}
+          options={{
+            tabBarIcon:() => (<Icon name="ios-home" size={25}/>)
+          }}/>
+          <Tab.Screen name="Search" component={SearchScreen}
+          options={{
+            tabBarIcon: () => (<Icon name="search" size={25}/>)
+          }}
+          />
+        </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,21 +4,7 @@ import homeStyles from '../styles/home';
 export default function HomeScreen({ navigation }){
     return(
         <View style={homeStyles.container}>
-            <Image source={require('../assets/002-restaurant.png')} style={homeStyles.image}/>
-            <Text style={homeStyles.title}>Restaurant Chooser</Text>
-            
-            <View style={homeStyles.navBox}>
-                <TouchableOpacity style={homeStyles.navItem} onPress={() => navigation.navigate('Search')}>
-                    <View>
-                        <Text style={homeStyles.navText}>View Restaurants</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={homeStyles.navItem} onPress={() => navigation.navigate('About')}>
-                    <View >
-                        <Text style={homeStyles.navText}>About</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
+            <Text>Dinery</Text>
         </View>
     )
 }
