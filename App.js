3bfac3b7896374a1c0251a8c0dbc9522da009c90
jasmynej14 <jasmynej14@brandeis.edu>
2021-10-25 12:59:18 -0400

    better profile screen, option for create account

--- App.js
+++ App.js
@@ -1,41 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import Restaurants from './screens/Restaurants';
 import Icon from 'react-native-vector-icons/AntDesign'
+import UserContext from './contexts/UserContext';
 import { TouchableOpacity, Text, View,Image } from 'react-native';
 const Tab = createBottomTabNavigator();
 export default function App({ route }) {
+  const [user,setUser] = useState({})
   return (
-    <NavigationContainer>
+    <UserContext.Provider value={[user,setUser]} >
+      <NavigationContainer>
        
-        <Tab.Navigator screenOptions={{
-          tabBarActiveTintColor:"red",
-          tabBarInactiveTintColor:"gray"
-        }}>
-          
-          <Tab.Screen name="Home" component={HomeScreen}
-          options={{
-            tabBarIcon:({focused}) => (<Icon name="home" size={25} color={focused? "red" : "gray"}/>),
-            headerShown:false
-          }}/>
-          <Tab.Screen name="Restaurants" component={Restaurants}
-          options={{
-            tabBarIcon: ({focused}) => (<Icon name="search1" size={25} color={focused? "red" : "gray"}/>),
-            headerShown:false
-          }}
-          />
-          <Tab.Screen name="Profile" component={ProfileScreen}
-            options={{
-              tabBarIcon: ({focused}) => (<Icon name="user" size={25} color={focused? "red" : "gray"}/>),
-              headerShown:false
-            }}
-          />
-        </Tab.Navigator>
-    </NavigationContainer>
+       <Tab.Navigator screenOptions={{
+         tabBarActiveTintColor:"red",
+         tabBarInactiveTintColor:"gray"
+       }}>
+         
+         <Tab.Screen name="Home" component={HomeScreen}
+         options={{
+           tabBarIcon:({focused}) => (<Icon name="home" size={25} color={focused? "red" : "gray"}/>),
+           headerShown:false
+         }}/>
+         <Tab.Screen name="Restaurants" component={Restaurants}
+         options={{
+           tabBarIcon: ({focused}) => (<Icon name="search1" size={25} color={focused? "red" : "gray"}/>),
+           headerShown:false
+         }}
+         />
+         <Tab.Screen name="Profile" component={ProfileScreen}
+           options={{
+             tabBarIcon: ({focused}) => (<Icon name="user" size={25} color={focused? "red" : "gray"}/>),
+             headerShown:false
+           }}
+         />
+       </Tab.Navigator>
+   </NavigationContainer>
+
+    </UserContext.Provider>
+    
   );
 }
 
--- screens/RestaurantInfo.js
+++ screens/RestaurantInfo.js
@@ -1,11 +1,13 @@
 import React, {useState,useEffect} from 'react';
 import { StyleSheet, Text, View,Button,TouchableOpacity,Image,SafeAreaView } from 'react-native';
 import TypeImage from '../components/TypeImage';
+import { useFonts } from 'expo-font';
+import { Quicksand_400Regular,Quicksand_300Light,Quicksand_600SemiBold} from '@expo-google-fonts/dev';
 import axios from 'axios';
 export default function RestaurantInfo({route}){
     const {restaurantID} = route.params
     const [restaurant,setRestaurant] = useState({})
-    
+    let [fontsLoaded] = useFonts({Quicksand_400Regular,Quicksand_300Light,Quicksand_600SemiBold})
     const getRestaurant = () => {
         axios.get(`https://dineryapi.herokuapp.com/restaurants/${restaurantID}`)
         .then(response => setRestaurant(response.data))
@@ -29,9 +31,17 @@ export default function RestaurantInfo({route}){
                 <Text style={styles.name}>{restaurant.name}</Text>
             </View>
             <View style={styles.infoBox}>
-                <Text>{restaurant.location.address}</Text>
-                <Text>{restaurant.location.city},{restaurant.location.state}</Text>
-                <Text>{restaurant.cuisine}</Text>
+                <Text style={styles.address}>{restaurant.location.address}</Text>
+                <Text style={styles.address}>{restaurant.location.city},{restaurant.location.state}</Text>
+            </View>
+            <View>
+                <Text>Compare</Text>
+            </View>
+            <View>
+                <Text>Menu</Text>
+            </View>
+            <View>
+                <Text>Reviews</Text>
             </View>
         </SafeAreaView>
     )
@@ -44,13 +54,18 @@ const styles = StyleSheet.create({
     },
     name:{
         fontSize:40,
-        fontWeight:"bold",
+        fontFamily:'Quicksand_600SemiBold',
         textAlign:"center"
     },
     infoBox:{
        backgroundColor:"#F7DC6F",
        borderRadius:10,
        padding:10,
-       margin:10 
+       margin:10,
+       textAlign:"center" 
+    },
+    address:{
+        fontFamily:'Quicksand_400Regular',
+        fontSize:20
     }
 })
